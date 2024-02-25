b0e0244d35bedc7bc51f665f014300c582346de4
Arinonia <arinonia.yt@gmail.com>
2024-02-25 22:23:24 +0100

    :tada: Starting project :)

---
+++ src/main/java/fr/arinonia/opensjgit/OpenSjGitApplication.java
@@ -0,0 +1,13 @@
+package fr.arinonia.opensjgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSjGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSjGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/arinonia/opensjgit/OpenSjGitApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arinonia.opensjgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSjGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
