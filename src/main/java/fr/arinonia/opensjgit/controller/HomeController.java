942c64d2cb705e06b9b44f537ee28c74449451d9
Arinonia <arinonia.yt@gmail.com>
2024-02-26 16:17:16 +0100

    login / register working and css look better! c:

--- src/main/java/fr/arinonia/opensjgit/controller/HomeController.java
+++ src/main/java/fr/arinonia/opensjgit/controller/HomeController.java
@@ -1,13 +1,19 @@
 package fr.arinonia.opensjgit.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String getHomeView() {
+    public String getHomeView(final Model model) {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        model.addAttribute("username", currentUserName);
         return "home";
     }
 }
