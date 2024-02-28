d6c67d374e67c683242954666357e922b05af9b9
tleulier <leulierthibaud@gmail.com>
2024-02-28 16:41:15 +0100

    :bug: fix home page from / route

--- src/main/java/fr/arinonia/opensjgit/controller/WebController.java
+++ src/main/java/fr/arinonia/opensjgit/controller/WebController.java
@@ -1,13 +1,28 @@
 package fr.arinonia.opensjgit.controller;
 
+import fr.arinonia.opensjgit.entity.User;
+import fr.arinonia.opensjgit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WebController {
+public class WebController implements ILoggedController {
+
+    private final UserService userService;
+
+    @Autowired
+    public WebController(final UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
-    public String getWebViw() {
+    public String getWebViw(final Model model) {
+        final User currentUser = this.getCurrentUser(this.userService);
+        model.addAttribute("creationDate", currentUser.getCreation_date());
+        model.addAttribute("profilePicPath", this.getProfilePicturePath(this.userService));
+        model.addAttribute("userRank", currentUser.getRank().name());
         return "home";
     }
 }
